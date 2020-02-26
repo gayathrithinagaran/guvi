8ea5666866107b4123db99f125f7f1fae06339fb
gayathrithinagaran <61046022+gayathrithinagaran@users.noreply.github.com>
2020-02-26 11:50:51 +0530

    Create countries_task.js

---
+++ javascript/countries_task.js
@@ -0,0 +1,28 @@
+#uppercase of countries capital#
+for(var i=0;i<300;i++)
+{
+var dum=(dummy[i].name);
+var s=dum.toUpperCase();
+console.log(s);
+}
+// filter capitals length to <5//
+const cplen=dummy.filter(item)=>{return(item.capital).length<=5}}
+console.log(cplen);
+// find a data of a country by using find method
+const finditem=dummy.find(item)=>{return item.name=='India'}}
+console.log(finditem)
+ # sum of all no's in array using reduce method#
+ var a=[1,2,3,4,5,6,7,89,10]
+ var sum=a.reduce(accumulator,currentvalue,index)
+ return accumulator+current value;
+ }};
+ console.log(sum)
+ #sum of odd no's in an array using reduce method#
+ var a=[1,2,3,4,5,6,7,8,9,10]
+ var sum=a.reduce function( accumulator,currentvalue,index){
+ if(currentvalue%2==0)
+ return accumulator+current value;
+ return accumulator;
+ }};
+ console.log(sum)
+ 
