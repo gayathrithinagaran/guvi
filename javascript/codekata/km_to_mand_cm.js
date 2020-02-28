273e3690ff8331990035047e4e331e237f06c2c2
gayathrithinagaran <61046022+gayathrithinagaran@users.noreply.github.com>
2020-02-28 10:56:17 +0530

    Create km_to_mand_cm.js

---
+++ javascript/codekata/km_to_mand_cm.js
@@ -0,0 +1,11 @@
+const readline=require('readline');
+const inp=readline.createInterface({
+  input:process.stdin});
+const n=[];
+inp.on("line",(data)=>{
+  n.push(data);});
+inp.on("close",()=>{
+  var a=n[0];
+  console.log(a*1000)
+  console.log(a*100000)
+});
