20244262962be68f8b9185225ac7491d7b9e49aa
gayathrithinagaran <61046022+gayathrithinagaran@users.noreply.github.com>
2020-03-09 11:29:46 +0530

    Create power of 2.py

---
+++ codekata/mathematics/power of 2.py
@@ -0,0 +1,15 @@
+const readline=require('readline');
+const inp=readline.createInterface({
+  input:process.stdin});
+var a;
+inp.on("line",(a)=>{
+  m=parseInt(a);
+  if(m%2==0)
+  {
+    console.log("yes");
+  }
+  else
+  {
+    console.log("no");
+  }
+  });
