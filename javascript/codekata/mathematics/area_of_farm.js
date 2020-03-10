e8a39225f853815e822af12896f9171759149ca2
gayathrithinagaran <61046022+gayathrithinagaran@users.noreply.github.com>
2020-03-10 12:25:01 +0530

    Create area_of_farm.js

---
+++ javascript/codekata/mathematics/area_of_farm.js
@@ -0,0 +1,11 @@
+const readline=require('readline');
+const inp=readline.createInterface({
+  input:process.stdin});
+const n=[];
+inp.on("line",(data)=>{
+  n.push(data);});
+inp.on("close",()=>{
+  var a=n[0].split(" "),A;
+  A=parseFloat(a[0])*parseFloat(a[1]);
+  console.log(A.toFixed(5));
+});
