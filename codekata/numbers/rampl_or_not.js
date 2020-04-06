1b23d94e857081664a7466793899ccb3ea4812db
gayathrithinagaran <61046022+gayathrithinagaran@users.noreply.github.com>
2020-04-06 18:48:12 +0530

    Create rampl_or_not.js

---
+++ codekata/numbers/rampl_or_not.js
@@ -0,0 +1,17 @@
+const readline=require('readline');
+const inp=readline.createInterface({
+  input:process.stdin});
+const n=[];
+inp.on("line",(data)=>{
+  n.push(data);});
+inp.on("close",()=>{
+  var a=n[0],sum;
+  if(parseInt(a)<0){
+    sum=parseInt(a[2])+parseInt(a[a.length-1]);
+  }
+  else{
+    sum=parseInt(a[a.length-2])+parseInt(a[a.length-1]); 
+  }
+  if(sum%4===0)console.log("yes");
+  else console.log("no");
+});
