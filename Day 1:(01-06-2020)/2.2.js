521e15e4f53ea2d312ce67cea94432e5fecf4f5c
gayathrithinagaran <61046022+gayathrithinagaran@users.noreply.github.com>
2020-06-01 21:00:22 +0530

    Create 2.2.js

---
+++ Day 1:(01-06-2020)/2.2.js
@@ -0,0 +1,20 @@
+//Insert a new key called email and assign a value email1@gmail.com and email2@gmail.com in both author details//
+author details: {
+name : "Raj";
+age : 40
+email : email1@gmail.com;
+email : email2@gmail.com;
+}
+},
+{
+title : "Nodejs",
+price : 600,
+reader : [],
+authorDetails : {
+         name : "Raj",
+         age :40:
+         email :email1@gmail.com;
+         email: email2@gmail.com ;
+         }
+         }
+      ]
