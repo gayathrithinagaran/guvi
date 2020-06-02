0cdedb87f6e0804a74869f42831d686169738db8
gayathrithinagaran <61046022+gayathrithinagaran@users.noreply.github.com>
2020-06-02 06:08:06 +0530

    Create 2.5.js

---
+++ Day 1:(01-06-2020)/2.5.js
@@ -0,0 +1,9 @@
+//function to create new object in library
+
+var findDataType = function(){
+console.log(typeof(library[0].authorDetails.age));
+}
+
+//call function findDataType
+
+findDataType();
