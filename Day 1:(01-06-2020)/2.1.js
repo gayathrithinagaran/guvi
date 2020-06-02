818f8318e1ba3caa30ae938a808a47322aac5b70
gayathrithinagaran <61046022+gayathrithinagaran@users.noreply.github.com>
2020-06-02 06:01:13 +0530

    Update 2.1.js

--- Day 1:(01-06-2020)/2.1.js
+++ Day 1:(01-06-2020)/2.1.js
@@ -1,1 +1,10 @@
 //update the count of Person 2 inside readers array in javascript//
+
+//function to update count in person2
+
+var update = function(data){
+library[0].readers[1].count=data;
+console.log(library[0].readers[1].count); // 400
+}
+
+update(400);
