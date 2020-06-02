739d5e1d2e6a4edbdd7385b3e8849b308e217104
gayathrithinagaran <61046022+gayathrithinagaran@users.noreply.github.com>
2020-06-02 07:17:06 +0530

    Update 2.3.js

--- Day 1:(01-06-2020)/2.3.js
+++ Day 1:(01-06-2020)/2.3.js
@@ -1,12 +1,13 @@
 //function to insert reader into readers in library
 
-var addReader = function(index,n,a){
-library[index].readers[library[index].readers.length]={"name":n,"count":a};
+var addReader = function(index,N,c)
+{
+library[index].readers[library[index].readers.length]={"name":N,"count":c};
 console.log(library[index].readers);
 }
 
-//assigning reader into readers in library
+//assigning newreader values into readers in library
 
-addReader(0,"Bala",300);
-addReader(1,"Bala",300);
-© 2020 GitHub, Inc.
+addReader(0,"Ramprasad",400);
+addReader(1,"Ramprasad",400);
+}
