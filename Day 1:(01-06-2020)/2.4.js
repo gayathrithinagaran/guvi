703b70995c4079cff9862f0e0596eddc982c2bdd
gayathrithinagaran <61046022+gayathrithinagaran@users.noreply.github.com>
2020-06-02 06:07:01 +0530

    Create 2.4.js

---
+++ Day 1:(01-06-2020)/2.4.js
@@ -0,0 +1,10 @@
+//function to create new object in library
+
+var addObject = function(ObjName){
+library[ObjName]={};
+console.log(library);
+}
+
+//assigning new object to library
+
+addObject("SQL");
