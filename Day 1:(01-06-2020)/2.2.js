8156c0c01fff43607b35991f876179135f0841e1
gayathrithinagaran <61046022+gayathrithinagaran@users.noreply.github.com>
2020-06-02 07:10:08 +0530

    Update 2.2.js

--- Day 1:(01-06-2020)/2.2.js
+++ Day 1:(01-06-2020)/2.2.js
@@ -1,12 +1,14 @@
 //Insert a new key called email and assign a value email1@gmail.com and email2@gmail.com in both author details//
+
+
 //function to add email to authorDetails in library
 
-var addEmail = function(index,emailId){
-library[index].authorDetails["email"]=emailId;
-console.log(library[index].authorDetails);
+var addEmail = function(Index,emailid){
+library[Index].authorDetails["email"]= emailid;
+Console.log(library[index].authorDetails);
 }
 
-//assigning email to authorDetails in library
+//ASSIGNING  A VALUE TO email1@gmail.com and email2@gmail.com
 
 addEmail(0,"email1@gmail.com");
 addEmail(1,"email2@gmail.com");
