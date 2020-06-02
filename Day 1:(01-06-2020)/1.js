c0abc58b213f8a64f26bb4d5a383f8d28e86da48
gayathrithinagaran <61046022+gayathrithinagaran@users.noreply.github.com>
2020-06-02 06:00:23 +0530

    Update 1.js

--- Day 1:(01-06-2020)/1.js
+++ Day 1:(01-06-2020)/1.js
@@ -1,1 +1,95 @@
+//1.
+
+//Creating TechnicalSpecification as Javascript Object to store given data in the image.
+
+var TechnicalSpecification = {
+    Dimensions:{
+        OverallLength:3500,
+        OverallWidth:1600,
+        OverallHeigth:1490,
+        WheelBase:2360,
+        TrackWidth:{Front:1405,Rear:1400},
+        MinimumTurningRadius:4.5,
+        MinimumGroungClearance:170
+    },
+    Capacities:{
+        SeatingCapacity:5,
+        FuelTankCapacity:35
+    },
+    Engine:{
+        Type:"KB-Series",
+        NumberOfCylinders:3,
+        NumberOfValves:12,
+        Capacity:998,
+        BoreScroke:"73.0 X 79.5",
+        CompressionRatio:"10:1",
+        MaximumPower:"67/6200",
+        MaximumTorQue:"90/3500",
+        FuelDistribution:"Multipoint Injection"
+    },
+    Transmission:{Type:"5-speed MT"},
+    Chassls:{
+        Steering:"Rack & Pinion, Power assisted",
+        Brakes:{Front:"Ventilated discs",Rear:"Drums"},
+        Suspension:{
+            Front:"MacPherson strut & coil spring",
+            Rear:"Isolated trailing link & coil spring"
+            
+        },
+        ShockAbsorbers:"Gas filled",
+        TypeTubeLess:"155/80R13"
+    },
+    Weights:{
+        KerbWeight:"860-880",
+        GrossVehicleWeight:1320
+    }
+};
+© 2020 GitHub, Inc.
+// 1. Convert specifications given below in image to Javascript Object;
+
+//Creating TechnicalSpecification as Javascript Object to store given data in the image.
+
+var TechnicalSpecification = {
+    Dimensions:{
+        OverallLength:3500,
+        OverallWidth:1600,
+        OverallHeigth:1490,
+        WheelBase:2360,
+        TrackWidth:{Front:1405,Rear:1400},
+        MinimumTurningRadius:4.5,
+        MinimumGroungClearance:170
+    },
+    Capacities:{
+        SeatingCapacity:5,
+        FuelTankCapacity:35
+    },
+    Engine:{
+        Type:"KB-Series",
+        NumberOfCylinders:3,
+        NumberOfValves:12,
+        Capacity:998,
+        BoreScroke:"73.0 X 79.5",
+        CompressionRatio:"10:1",
+        MaximumPower:"67/6200",
+        MaximumTorQue:"90/3500",
+        FuelDistribution:"Multipoint Injection"
+    },
+    Transmission:{Type:"5-speed MT"},
+    Chassls:{
+        Steering:"Rack & Pinion, Power assisted",
+        Brakes:{Front:"Ventilated discs",Rear:"Drums"},
+        Suspension:{
+            Front:"MacPherson strut & coil spring",
+            Rear:"Isolated trailing link & coil spring"
+            
+        },
+        ShockAbsorbers:"Gas filled",
+        TypeTubeLess:"155/80R13"
+    },
+    Weights:{
+        KerbWeight:"860-880",
+        GrossVehicleWeight:1320
+    }
+};
+
 
