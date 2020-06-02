d17ad58fb196537822d10b5b787412f57e37c451
gayathrithinagaran <61046022+gayathrithinagaran@users.noreply.github.com>
2020-06-02 07:19:52 +0530

    Update 2.4.js

--- Day 1:(01-06-2020)/2.4.js
+++ Day 1:(01-06-2020)/2.4.js
@@ -1,10 +1,10 @@
 //function to create new object in library
 
-var addObject = function(ObjName){
-library[ObjName]={};
+var AddObject = function(objectName){
+library[objectName]= {};
 console.log(library);
 }
 
 //assigning new object to library
 
-addObject("SQL");
+AddObject("SQL");
