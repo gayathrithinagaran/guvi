69e65a21d6c7ad223df0ca5bbe69c3d929242995
gayathrithinagaran <61046022+gayathrithinagaran@users.noreply.github.com>
2020-06-02 07:31:20 +0530

    Update 2.6.js

--- Day 1:(01-06-2020)/2.6.js
+++ Day 1:(01-06-2020)/2.6.js
@@ -1,13 +1,15 @@
 //function to print Price of specific book in library
 
-var printPrice = function(bookName){
-for(var i=0;i<library.length;i++){
-if(library[i].title===bookName){
+var printPrice = function(bookName)
+{
+for(var j=0;j<library.length;j++){
+if(library[j].title===bookName) 
+{
 console.log(library[i].price);
 break;
 }
-}}
+}
+}
 //call function printPrice
 
 printPrice("Javascript");
-© 2020 GitHub, Inc.
