770cbf26ac28195bbe6c8e3312430ae2127f65a6
gayathrithinagaran <61046022+gayathrithinagaran@users.noreply.github.com>
2020-06-03 19:38:24 +0530

    Update 2.3.js

--- Day3:(03-06-2020)/2.js/2.3.js
+++ Day3:(03-06-2020)/2.js/2.3.js
@@ -1,1 +1,10 @@
 //Sum of all numbers in an array//
+
+var b = function(a) {
+    sum = 0            //initialize sum to zero//
+    for (var i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    console.log(sum);
+}
+b(arr);
