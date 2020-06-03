166104960cc6ac41d90303b4a871eeb1269e057f
gayathrithinagaran <61046022+gayathrithinagaran@users.noreply.github.com>
2020-06-03 19:42:57 +0530

    Delete 2.1.js

--- Day3:(03-06-2020)/2.1.js
+++
@@ -1,7 +0,0 @@
-//Print odd numbers in an array//
-
-arr = [1,2,3,4,5,6,7,8,9,10,11,12]
-
- odd = arr.filter(n => n%2) //filter odd numbers in a arr//
-
-console.log(odd)   //print odd numbers in arr//
