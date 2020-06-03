8a039f8eaae47655e6d1d3e0070c0c8f7b6d3d74
gayathrithinagaran <61046022+gayathrithinagaran@users.noreply.github.com>
2020-06-03 19:31:46 +0530

    Update 2.2.js

--- Day3:(03-06-2020)/2.js/2.2.js
+++ Day3:(03-06-2020)/2.js/2.2.js
@@ -1,1 +1,9 @@
-//Convert all the strings to title caps in a string array//
+//convert all strings to title cap using anonymous function
+
+var b = function(a) {
+    for (var i = 0; i < arr.length; i++) 
+    {
+        console.log(arr[i][0].toUpperCase() + arr[i].substr(1)); 
+    }
+}
+b(arr);
