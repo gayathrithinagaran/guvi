8c0457fd6d81b566ff87545008364ac5b8c57db9
gayathrithinagaran <61046022+gayathrithinagaran@users.noreply.github.com>
2020-06-04 16:39:11 +0530

    Update 2.7.js

--- Day3:(03-06-2020)/2.js/2.7.js
+++ Day3:(03-06-2020)/2.js/2.7.js
@@ -1,1 +1,11 @@
+//2.7 Removes duplicates from array
+
+var arr=[32,50,32,75,38]
+var dupli= function(arr){
+  
+var Result=[...new Set(arr)];
+return Result;
+}
+console.log(dupli(arr));// remiove and print all duplicates//
+}
 
