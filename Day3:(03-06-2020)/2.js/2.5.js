9cc0f72011967d03468f7b4c7ed25719df16fa45
gayathrithinagaran <61046022+gayathrithinagaran@users.noreply.github.com>
2020-06-04 16:44:19 +0530

    Update 2.5.js

--- Day3:(03-06-2020)/2.js/2.5.js
+++ Day3:(03-06-2020)/2.js/2.5.js
@@ -1,1 +1,15 @@
+//2.5 Return of all the palindromes in an array
 
+var string=["level","noon","radar"]
+var palindrome = function () {
+var result=string.filter(function(t)
+{
+var a=t.split('').reverse().join('')
+if(t==a)
+{
+return t;
+}
+})
+console.log(result);
+};
+palindrome()
