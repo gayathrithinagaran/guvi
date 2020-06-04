c4a448c7ac96c672f24ab18a15cf51b70654d83d
gayathrithinagaran <61046022+gayathrithinagaran@users.noreply.github.com>
2020-06-04 06:22:33 +0530

    Create 4.5.js

---
+++ Day3:(03-06-2020)/2.js/4/4.5.js
@@ -0,0 +1,12 @@
+//Return all prime numbersa in an array//
+var newArray = [1, 3, 2, 5, 10];
+const isPrime = num => {
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) return false;
+  }
+  return num !== 1;
+};
+
+
+const myPrimeArray = newArray.filter(element => isPrime(element));
+console.log(myPrimeArray);
