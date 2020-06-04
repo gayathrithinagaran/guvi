87565d636b26b3454913603616823a8de5a9c247
gayathrithinagaran <61046022+gayathrithinagaran@users.noreply.github.com>
2020-06-04 16:35:04 +0530

    Update 2.8.js

--- Day3:(03-06-2020)/2.js/2.8.js
+++ Day3:(03-06-2020)/2.js/2.8.js
@@ -1,1 +1,13 @@
+//2.8 Return an array by K times and return the rotated array//
 
+var arr=[34,56,38,29,19],K=3;
+var rev=function(array,k){
+  
+for(var i=0;i<K;i++)
+{
+var a= arr.Shift();
+array.push(a);
+}
+return arr;
+}
+console.log(rev(arr,K));//Return rotated arr//
