3a515f862095a378b664265c94fa1fb307633f6e
gayathrithinagaran <61046022+gayathrithinagaran@users.noreply.github.com>
2020-06-04 06:17:28 +0530

    Update 4.2.js

--- Day3:(03-06-2020)/2.js/4/4.2.js
+++ Day3:(03-06-2020)/2.js/4/4.2.js
@@ -1,1 +1,12 @@
 //convert all the strings to title caps in a string array//
+
+var arr=["pop","push","stack","data"];
+
+var titleCaps=arr.map(word=>{
+   var string = word[0].toUpperCase();
+   for(var i=1;i<word.length;i++)string+=word[i].toLowerCase();
+   return string;
+    
+}
+ );
+console.log(titleCaps);
