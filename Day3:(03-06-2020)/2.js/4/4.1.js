ca07d7781bba7d445379a22b0d5530605ae31e2f
gayathrithinagaran <61046022+gayathrithinagaran@users.noreply.github.com>
2020-06-04 06:00:48 +0530

    Update 4.1.js

--- Day3:(03-06-2020)/2.js/4/4.1.js
+++ Day3:(03-06-2020)/2.js/4/4.1.js
@@ -1,4 +1,6 @@
-var array=[1,2,3,4,5,6,7,8,9,10];
-//arrow function to filter odd numbers
+//Print odd numbers in an array//
+
+var array=[1,2,3,4,5,6,7,8,9,10,11,12];
+
 var oddNumber=array.filter(num=>(num%2)!==0);
-console.log(oddNumber)//[ 1, 3, 5, 7, 9 ]
+console.log(oddNumber); //print odd num in array
