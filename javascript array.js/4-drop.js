9c98b022dba1c8a397f0ae41c89be27a54ef3170
gayathrithinagaran <61046022+gayathrithinagaran@users.noreply.github.com>
2020-06-07 17:53:11 +0530

    Create 4-drop.js

---
+++ javascript array.js/4-drop.js
@@ -0,0 +1,9 @@
+//4.Creates a slice of array with n elements dropped from the beginning//
+
+
+function drop(array)
+{
+    var slicearr=array.slice(1,3);
+    console.log(slicearr); //returns slice of array
+}
+drop([1, 2, 3]) 
